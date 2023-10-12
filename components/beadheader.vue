f60030228bd40e69063969a51fdc1443959e3be6
boranbatuhan <boranbatuh@gmail.com>
2023-10-12 15:54:23 +0300

    Initial commit

---
+++ components/beadheader.vue
@@ -0,0 +1,330 @@
+<template>
+<div id="beadcontainer" class="h-fit w-full">
+    <div class="line1 flex w-full items-center justify-center relative">
+        <div class="h-px w-full bg-gradient-to-r from-transparent via-black/40 to-transparent absolute"></div>
+        <div class="bead bead-orange" style="--i:1"></div>
+        <div class="bead bead-orange" style="--i:2"></div>
+        <div class="bead bead-orange" style="--i:3"></div>
+        <div class="bead bead-orange" style="--i:4"></div>
+        <div class="bead bead-orange" style="--i:5"></div>
+        <div class="bead bead-orange" style="--i:6"></div>
+        <div class="bead bead-orange" style="--i:7"></div>
+        <div class="bead bead-orange" style="--i:8"></div>
+        <div class="bead bead-orange" style="--i:9"></div>
+        <div class="bead bead-orange" style="--i:10"></div>
+        <div class="bead bead-orange" style="--i:11"></div>
+        <div class="bead bead-orange" style="--i:12"></div>
+        <div class="bead bead-orange" style="--i:13"></div>
+        <div class="bead bead-orange" style="--i:14"></div>
+        <div class="bead bead-orange" style="--i:15"></div>
+        <div class="bead bead-orange" style="--i:16"></div>
+        <div class="bead bead-orange" style="--i:17"></div>
+        <div class="bead bead-orange" style="--i:18"></div>
+        <div class="bead bead-orange" style="--i:19"></div>
+        <div class="bead bead-orange" style="--i:20"></div>
+        <div class="bead bead-orange" style="--i:21"></div>
+        <div class="bead bead-orange" style="--i:22"></div>
+        <div class="bead bead-orange" style="--i:23"></div>
+        <div class="bead bead-orange" style="--i:24"></div>
+        <div class="bead bead-orange" style="--i:25"></div>
+        <div class="bead bead-orange" style="--i:26"></div>
+        <div class="bead bead-orange" style="--i:27"></div>
+        <div class="bead bead-orange" style="--i:28"></div>
+        <div class="bead bead-orange" style="--i:29"></div>
+        <div class="bead bead-orange" style="--i:30"></div>
+        <div class="bead bead-orange" style="--i:31"></div>
+        <div class="bead bead-orange" style="--i:32"></div>
+        <div class="bead bead-orange" style="--i:33"></div>
+        <div class="bead bead-orange" style="--i:34"></div>
+        <div class="bead bead-orange" style="--i:35"></div>
+        <div class="bead bead-orange" style="--i:36"></div>
+
+    </div>
+    <div class="line2 flex w-full items-center justify-center relative">
+        <div class="h-px w-full bg-gradient-to-r from-transparent via-black/40 to-transparent absolute"></div>
+        <div class="bead bead-orange" style="--i:1.4"></div>
+        <div class="bead bead-black" style="--i:2.4"></div>
+        <div class="bead bead-orange" style="--i:3.4"></div>
+        <div class="bead bead-orange" style="--i:4.4"></div>
+        <div class="bead bead-black" style="--i:5.4"></div>
+        <div class="bead bead-orange" style="--i:6.4"></div>
+        <div class="bead bead-black" style="--i:7.4"></div>
+        <div class="bead bead-orange" style="--i:8.4"></div>
+        <div class="bead bead-orange" style="--i:9.4"></div>
+        <div class="bead bead-orange" style="--i:10.4"></div>
+        <div class="bead bead-black" style="--i:11.4"></div>
+        <div class="bead bead-orange" style="--i:12.4"></div>
+        <div class="bead bead-black" style="--i:13.4"></div>
+        <div class="bead bead-orange" style="--i:14.4"></div>
+        <div class="bead bead-orange" style="--i:15.4"></div>
+        <div class="bead bead-black" style="--i:16.4"></div>
+        <div class="bead bead-orange" style="--i:17.4"></div>
+        <div class="bead bead-black" style="--i:18.4"></div>
+        <div class="bead bead-black" style="--i:19.4"></div>
+        <div class="bead bead-black" style="--i:20.4"></div>
+        <div class="bead bead-black" style="--i:21.4"></div>
+        <div class="bead bead-orange" style="--i:22.4"></div>
+        <div class="bead bead-black" style="--i:23.4"></div>
+        <div class="bead bead-black" style="--i:24.4"></div>
+        <div class="bead bead-black" style="--i:25.4"></div>
+        <div class="bead bead-orange" style="--i:26.4"></div>
+        <div class="bead bead-orange" style="--i:27.4"></div>
+        <div class="bead bead-black" style="--i:28.4"></div>
+        <div class="bead bead-black" style="--i:29.4"></div>
+        <div class="bead bead-black" style="--i:30.4"></div>
+        <div class="bead bead-orange" style="--i:31.4"></div>
+        <div class="bead bead-black" style="--i:32.4"></div>
+        <div class="bead bead-black" style="--i:33.4"></div>
+        <div class="bead bead-black" style="--i:34.4"></div>
+        <div class="bead bead-black" style="--i:35.4"></div>
+        <div class="bead bead-orange" style="--i:36.4"></div>
+    </div>
+    <div class="line3 flex w-full items-center justify-center relative">
+        <div class="h-px w-full bg-gradient-to-r from-transparent via-black/40 to-transparent absolute"></div>
+        <div class="bead bead-orange" style="--i:1.7"></div>
+        <div class="bead bead-black" style="--i:2.7"></div>
+        <div class="bead bead-orange" style="--i:3.7"></div>
+        <div class="bead bead-orange" style="--i:4.7"></div>
+        <div class="bead bead-black" style="--i:5.7"></div>
+        <div class="bead bead-orange" style="--i:6.7"></div>
+        <div class="bead bead-black" style="--i:7.7"></div>
+        <div class="bead bead-black" style="--i:8.7"></div>
+        <div class="bead bead-orange" style="--i:9.7"></div>
+        <div class="bead bead-orange" style="--i:10.7"></div>
+        <div class="bead bead-black" style="--i:11.7"></div>
+        <div class="bead bead-orange" style="--i:12.7"></div>
+        <div class="bead bead-black" style="--i:13.7"></div>
+        <div class="bead bead-orange" style="--i:14.7"></div>
+        <div class="bead bead-orange" style="--i:15.7"></div>
+        <div class="bead bead-black" style="--i:16.7"></div>
+        <div class="bead bead-orange" style="--i:17.7"></div>
+        <div class="bead bead-black" style="--i:18.7"></div>
+        <div class="bead bead-orange" style="--i:19.7"></div>
+        <div class="bead bead-orange" style="--i:20.7"></div>
+        <div class="bead bead-orange" style="--i:21.7"></div>
+        <div class="bead bead-orange" style="--i:22.7"></div>
+        <div class="bead bead-black" style="--i:23.7"></div>
+        <div class="bead bead-orange" style="--i:24.7"></div>
+        <div class="bead bead-orange" style="--i:25.7"></div>
+        <div class="bead bead-black" style="--i:26.7"></div>
+        <div class="bead bead-orange" style="--i:27.7"></div>
+        <div class="bead bead-black" style="--i:28.7"></div>
+        <div class="bead bead-orange" style="--i:29.7"></div>
+        <div class="bead bead-orange" style="--i:30.7"></div>
+        <div class="bead bead-orange" style="--i:31.7"></div>
+        <div class="bead bead-black" style="--i:32.7"></div>
+        <div class="bead bead-orange" style="--i:33.7"></div>
+        <div class="bead bead-orange" style="--i:34.7"></div>
+        <div class="bead bead-orange" style="--i:35.7"></div>
+        <div class="bead bead-orange" style="--i:36.7"></div>
+    </div>
+    <div class="line4 flex w-full items-center justify-center relative">
+        <div class="h-px w-full bg-gradient-to-r from-transparent via-black/40 to-transparent absolute"></div>
+        <div class="bead bead-orange" style="--i:2"></div>
+        <div class="bead bead-black" style="--i:3"></div>
+        <div class="bead bead-orange" style="--i:4"></div>
+        <div class="bead bead-orange" style="--i:5"></div>
+        <div class="bead bead-black" style="--i:6"></div>
+        <div class="bead bead-orange" style="--i:7"></div>
+        <div class="bead bead-black" style="--i:8"></div>
+        <div class="bead bead-orange" style="--i:9"></div>
+        <div class="bead bead-black" style="--i:10"></div>
+        <div class="bead bead-orange" style="--i:11"></div>
+        <div class="bead bead-black" style="--i:12"></div>
+        <div class="bead bead-orange" style="--i:13"></div>
+        <div class="bead bead-black" style="--i:14"></div>
+        <div class="bead bead-orange" style="--i:15"></div>
+        <div class="bead bead-orange" style="--i:16"></div>
+        <div class="bead bead-black" style="--i:17"></div>
+        <div class="bead bead-orange" style="--i:18"></div>
+        <div class="bead bead-orange" style="--i:19"></div>
+        <div class="bead bead-black" style="--i:20"></div>
+        <div class="bead bead-black" style="--i:21"></div>
+        <div class="bead bead-orange" style="--i:22"></div>
+        <div class="bead bead-orange" style="--i:23"></div>
+        <div class="bead bead-black" style="--i:24"></div>
+        <div class="bead bead-black" style="--i:25"></div>
+        <div class="bead bead-black" style="--i:26"></div>
+        <div class="bead bead-orange" style="--i:27"></div>
+        <div class="bead bead-orange" style="--i:28"></div>
+        <div class="bead bead-black" style="--i:29"></div>
+        <div class="bead bead-black" style="--i:30"></div>
+        <div class="bead bead-orange" style="--i:31"></div>
+        <div class="bead bead-orange" style="--i:32"></div>
+        <div class="bead bead-orange" style="--i:33"></div>
+        <div class="bead bead-black" style="--i:34"></div>
+        <div class="bead bead-black" style="--i:35"></div>
+        <div class="bead bead-orange" style="--i:36"></div>
+        <div class="bead bead-orange" style="--i:37"></div>
+    </div>
+    <div class="line5 flex w-full items-center justify-center relative">
+        <div class="h-px w-full bg-gradient-to-r from-transparent via-black/40 to-transparent absolute"></div>
+        <div class="bead bead-orange" style="--i:2.4"></div>
+        <div class="bead bead-black" style="--i:3.4"></div>
+        <div class="bead bead-orange" style="--i:4.4"></div>
+        <div class="bead bead-orange" style="--i:5.4"></div>
+        <div class="bead bead-black" style="--i:6.4"></div>
+        <div class="bead bead-orange" style="--i:7.4"></div>
+        <div class="bead bead-black" style="--i:8.4"></div>
+        <div class="bead bead-orange" style="--i:9.4"></div>
+        <div class="bead bead-orange" style="--i:10.4"></div>
+        <div class="bead bead-black" style="--i:11.4"></div>
+        <div class="bead bead-black" style="--i:12.4"></div>
+        <div class="bead bead-orange" style="--i:13.4"></div>
+        <div class="bead bead-black" style="--i:14.4"></div>
+        <div class="bead bead-orange" style="--i:15.4"></div>
+        <div class="bead bead-orange" style="--i:16.4"></div>
+        <div class="bead bead-black" style="--i:17.4"></div>
+        <div class="bead bead-orange" style="--i:18.4"></div>
+        <div class="bead bead-orange" style="--i:19.4"></div>
+        <div class="bead bead-orange" style="--i:20.4"></div>
+        <div class="bead bead-orange" style="--i:21.4"></div>
+        <div class="bead bead-black" style="--i:22.4"></div>
+        <div class="bead bead-orange" style="--i:23.4"></div>
+        <div class="bead bead-black" style="--i:24.4"></div>
+        <div class="bead bead-orange" style="--i:25.4"></div>
+        <div class="bead bead-orange" style="--i:26.4"></div>
+        <div class="bead bead-orange" style="--i:27.4"></div>
+        <div class="bead bead-orange" style="--i:28.4"></div>
+        <div class="bead bead-black" style="--i:29.4"></div>
+        <div class="bead bead-orange" style="--i:30.4"></div>
+        <div class="bead bead-orange" style="--i:31.4"></div>
+        <div class="bead bead-orange" style="--i:32.4"></div>
+        <div class="bead bead-orange" style="--i:33.4"></div>
+        <div class="bead bead-orange" style="--i:34.4"></div>
+        <div class="bead bead-orange" style="--i:35.4"></div>
+        <div class="bead bead-black" style="--i:36.4"></div>
+        <div class="bead bead-orange" style="--i:37.4"></div>
+    </div>
+    <div class="line6 flex w-full items-center justify-center relative">
+        <div class="h-px w-full bg-gradient-to-r from-transparent via-black/40 to-transparent absolute"></div>
+        <div class="bead bead-orange" style="--i:2.7"></div>
+        <div class="bead bead-black" style="--i:3.7"></div>
+        <div class="bead bead-black" style="--i:4.7"></div>
+        <div class="bead bead-black" style="--i:5.7"></div>
+        <div class="bead bead-black" style="--i:6.7"></div>
+        <div class="bead bead-orange" style="--i:7.7"></div>
+        <div class="bead bead-black" style="--i:8.7"></div>
+        <div class="bead bead-orange" style="--i:9.7"></div>
+        <div class="bead bead-orange" style="--i:10.7"></div>
+        <div class="bead bead-orange" style="--i:11.7"></div>
+        <div class="bead bead-black" style="--i:12.7"></div>
+        <div class="bead bead-orange" style="--i:13.7"></div>
+        <div class="bead bead-black" style="--i:14.7"></div>
+        <div class="bead bead-black" style="--i:15.7"></div>
+        <div class="bead bead-black" style="--i:16.7"></div>
+        <div class="bead bead-black" style="--i:17.7"></div>
+        <div class="bead bead-orange" style="--i:18.7"></div>
+        <div class="bead bead-black" style="--i:19.7"></div>
+        <div class="bead bead-black" style="--i:20.7"></div>
+        <div class="bead bead-black" style="--i:21.7"></div>
+        <div class="bead bead-black" style="--i:22.7"></div>
+        <div class="bead bead-orange" style="--i:23.7"></div>
+        <div class="bead bead-black" style="--i:24.7"></div>
+        <div class="bead bead-orange" style="--i:25.7"></div>
+        <div class="bead bead-orange" style="--i:26.7"></div>
+        <div class="bead bead-orange" style="--i:27.7"></div>
+        <div class="bead bead-orange" style="--i:28.7"></div>
+        <div class="bead bead-black" style="--i:29.7"></div>
+        <div class="bead bead-black" style="--i:30.7"></div>
+        <div class="bead bead-black" style="--i:31.7"></div>
+        <div class="bead bead-orange" style="--i:32.7"></div>
+        <div class="bead bead-black" style="--i:33.7"></div>
+        <div class="bead bead-black" style="--i:34.7"></div>
+        <div class="bead bead-black" style="--i:35.7"></div>
+        <div class="bead bead-black" style="--i:36.7"></div>
+        <div class="bead bead-orange" style="--i:37.7"></div>
+    </div>
+    <div class="line7 flex w-full items-center justify-center relative">
+        <div class="h-px w-full bg-gradient-to-r from-transparent via-black/40 to-transparent absolute"></div>
+        <div class="bead bead-orange" style="--i:3"></div>
+        <div class="bead bead-orange" style="--i:4"></div>
+        <div class="bead bead-orange" style="--i:5"></div>
+        <div class="bead bead-orange" style="--i:6"></div>
+        <div class="bead bead-orange" style="--i:7"></div>
+        <div class="bead bead-orange" style="--i:8"></div>
+        <div class="bead bead-orange" style="--i:9"></div>
+        <div class="bead bead-orange" style="--i:10"></div>
+        <div class="bead bead-orange" style="--i:11"></div>
+        <div class="bead bead-orange" style="--i:12"></div>
+        <div class="bead bead-orange" style="--i:13"></div>
+        <div class="bead bead-orange" style="--i:14"></div>
+        <div class="bead bead-orange" style="--i:15"></div>
+        <div class="bead bead-orange" style="--i:16"></div>
+        <div class="bead bead-orange" style="--i:17"></div>
+        <div class="bead bead-orange" style="--i:18"></div>
+        <div class="bead bead-orange" style="--i:19"></div>
+        <div class="bead bead-orange" style="--i:20"></div>
+        <div class="bead bead-orange" style="--i:21"></div>
+        <div class="bead bead-orange" style="--i:22"></div>
+        <div class="bead bead-orange" style="--i:23"></div>
+        <div class="bead bead-orange" style="--i:24"></div>
+        <div class="bead bead-orange" style="--i:25"></div>
+        <div class="bead bead-orange" style="--i:26"></div>
+        <div class="bead bead-orange" style="--i:27"></div>
+        <div class="bead bead-orange" style="--i:28"></div>
+        <div class="bead bead-orange" style="--i:29"></div>
+        <div class="bead bead-orange" style="--i:30"></div>
+        <div class="bead bead-orange" style="--i:31"></div>
+        <div class="bead bead-orange" style="--i:32"></div>
+        <div class="bead bead-orange" style="--i:33"></div>
+        <div class="bead bead-orange" style="--i:34"></div>
+        <div class="bead bead-orange" style="--i:35"></div>
+        <div class="bead bead-orange" style="--i:36"></div>
+        <div class="bead bead-orange" style="--i:37"></div>
+        <div class="bead bead-orange" style="--i:38"></div>
+    </div>
+    
+</div>
+</template>
+
+<script setup>
+
+
+</script>
+
+<style>
+
+.bead-black{
+    background-image: linear-gradient(180deg, #ffffff 0%, #2b2225 2%, #110a08 98%, #000000 100%);
+
+}
+.bead-white{
+    background-image: linear-gradient(180deg, #ffffff 0%, #ffdfec 2%, #b3b3b3 98%, #000000 100%);
+
+}
+.bead-orange{
+    background-image: linear-gradient(180deg, #ffffff 0%, #ff9742 2%, #ff5c21 98%, #000000 100%);
+
+}
+
+.bead{
+    width: 1rem;
+    height: 1rem;
+    border-radius: 15%;
+    margin: 1px;
+    filter: drop-shadow(1px 1px 2px black);
+    user-select: none;
+    animation: slide-top 0.5s ease-in-out forwards   ;
+    animation-delay: calc(var(--i) * 100ms);
+    transform: translateX(calc( 100vw + 100px));
+    flex-shrink: 0;
+}
+.bead::before{
+    content: "";
+    width: .3rem;
+    height: .3rem;
+    background: #ffffff;
+    position: absolute;
+    top: 1px;
+    left: 1px;
+    filter: blur(3px);
+}
+@keyframes slide-top {
+  0% {
+  }
+  100% {
+    transform: translateX(0px);
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<script setup>
+</script>
+
+
+<template>
+    <div class="container overflow-x-hidden flex items-center justify-start flex-col mx-auto border border-black min-h-screen py-2">
+        <beadheader class="hue"/>
+    </div>
+</template>
+
+
+<style>
+.hues{
+    animation: huec 5s infinite linear alternate;
+}
+
+@keyframes huec {
+    0%{
+        filter: hue-rotate(0);
+    }
+    100%{
+        filter: hue-rotate(1turn);
+    }
+}
+</style>
